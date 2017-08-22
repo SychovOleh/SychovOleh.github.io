b3f293f519018b12740a16a4d9c8233b73acd06e
SychevOleh <sychovoleh@gmail.com>
2017-08-22 18:33:25 +0300

    started to add bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 7 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./dest'))
+})
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./src/styles/**/*.scss', ['sass']);
+});
