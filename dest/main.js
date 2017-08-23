53ccce71c008f2308b2b8d88ef5a544ce5ba1581
SychevOleh <sychovoleh@gmail.com>
2017-08-23 23:09:47 +0300

    part of items finished

--- dest/main.js
+++ dest/main.js
@@ -1,8 +1,8 @@
 // console.log($)
-$(function() {
-  // debugger
-  let imgPaddWidth = Number($('.work__img').eq(0).css('padding-right').replace('px', '')) +
-    Number($('.work__img').eq(0).css('padding-left').replace('px', ''));
-  let contWidth = $('.work__img').eq(0).width();
-  $('.work__img').css('height', (contWidth + imgPaddWidth) * .5625); // images aspect ratio 16:9
-})
+// $(function() {
+//   // debugger
+//   let imgPaddWidth = Number($('.work__img').eq(0).css('padding-right').replace('px', '')) +
+//     Number($('.work__img').eq(0).css('padding-left').replace('px', ''));
+//   let contWidth = $('.work__img').eq(0).width();
+//   $('.work__img').css('height', (contWidth + imgPaddWidth) * .5625); // images aspect ratio 16:9
+// })
