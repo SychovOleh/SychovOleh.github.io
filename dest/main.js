524accbea469f5255f1cffcd34ffe4eed4e9ae35
SychevOleh <sychovoleh@gmail.com>
2017-08-26 13:42:21 +0300

    found necessary logos and started to add them

---
+++ dest/main.js
@@ -0,0 +1,18 @@
+(function() {
+  const jumboSizing = () => {
+    let viewH = $(window).height();
+    let jumboH = $('.jumbotron').height();
+    let differH = viewH - jumboH;
+    $('.jumbotron').css({
+      paddingTop: differH / 2 - 20,
+      paddingBottom: differH / 2 + 20,
+    })
+  }
+  window.jumboSizing = jumboSizing;
+
+})()
+
+$(function() {
+  jumboSizing();
+  // $(window).resize(jumboSizing())
+})
