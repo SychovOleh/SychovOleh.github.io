702c993a53f17cc9639f7e3e41c7aab4a4ca9382
SychevOleh <sychovoleh@gmail.com>
2017-08-26 23:58:04 +0300

    little more logos to works, added image for form-validation

--- dest/main.js
+++ dest/main.js
@@ -1,17 +1,14 @@
-(function() {
-  const jumboSizing = () => {
-    let viewH = $(window).height();
-    let jumboH = $('.jumbotron').height();
-    let differH = viewH - jumboH;
-    $('.jumbotron').css({
-      paddingTop: differH / 2 - 20,
-      paddingBottom: differH / 2 + 20,
-    })
-  }
-  window.jumboSizing = jumboSizing;
-
-})()
+const heightSizingByPad = (sizingEl, regardingWhichEl, offsetRelatMiddleView = 0) => {
+  let futureH = $(sizingEl).height();
+  let regardingH = $(regardingWhichEl).height();
+  let differH = regardingH - futureH;
+  $(sizingEl).css({
+    paddingTop: differH / 2 - offsetRelatMiddleView,
+    paddingBottom: differH / 2 + offsetRelatMiddleView,
+  }) 
+}
 
 $(function() {
-  jumboSizing();
+  heightSizingByPad($('.jumbotron'), $(window), 20)
+  let a = $('#work').offset().top;
 })
