2e6bbb9fba11049c8b8daa1256182603a4af7cf2
SychevOleh <sychovoleh@gmail.com>
2017-08-27 15:41:50 +0300

    fixed dog link

--- dest/main.js
+++ dest/main.js
@@ -1,14 +1,22 @@
-const heightSizingByPad = (sizingEl, regardingWhichEl, offsetRelatMiddleView = 0) => {
-  let futureH = $(sizingEl).height();
-  let regardingH = $(regardingWhichEl).height();
-  let differH = regardingH - futureH;
+const heightSizingByPad = (sizingEl, relativeWhich, offsetRelatMiddleView = 0, addingsToChangingEl = 0) => {
+  if (typeof addingsToChangingEl === 'object') {
+    addingsToChangingEl = $(addingsToChangingEl).height();
+  }
+  const futureH = $(sizingEl).height() + addingsToChangingEl;
+
+  let relativeH;
+  if (typeof relativeWhich === 'number') {
+    relativeH = relativeWhich;
+  } else { relativeH = $(relativeWhich).height() }
+
+  const differH = relativeH - futureH;
   $(sizingEl).css({
     paddingTop: differH / 2 - offsetRelatMiddleView,
     paddingBottom: differH / 2 + offsetRelatMiddleView,
-  }) 
+  })
 }
 
 $(function() {
-  heightSizingByPad($('.jumbotron'), $(window), 20)
-  let a = $('#work').offset().top;
+  heightSizingByPad($('.header'), $(window), 20)
+    // let a = $('#work').offset().top;
 })
