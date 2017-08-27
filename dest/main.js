712c50f001622fcce6192175371987faa9c63c38
SychevOleh <sychovoleh@gmail.com>
2017-08-27 17:38:08 +0300

    changed CDN to Google again

--- dest/main.js
+++ dest/main.js
@@ -1,13 +1,27 @@
-const heightSizingByPad = (sizingEl, relativeWhich, offsetRelatMiddleView = 0, addingsToChangingEl = 0) => {
-  if (typeof addingsToChangingEl === 'object') {
-    addingsToChangingEl = $(addingsToChangingEl).height();
-  }
-  const futureH = $(sizingEl).height() + addingsToChangingEl;
+const findMargVert = (el) => {
+  return Number($(el).css('margin-top').replace('px', '')) + Number($(el).css('margin-bottom').replace('px', ''));
+}
+const findPadVert = (el) => {
+  return Number($(el).css('padding-top').replace('px', '')) + Number($(el).css('padding-bottom').replace('px', ''));
+}
 
+const heightSizingByPad = (sizingEl, relativeWhich, offsetRelatMiddleView = 0, addingsToChangingEl = 0) => {
+  // sizingEl: ELEMENT. That will change height
+  // relativeWhich: ELEMENT or NUMBER(heght-size)
+  // offsetRelatMiddleView: NUMBER. Shift sizingEl by vertical. if +N => shift top/ if -N => bot.
+  // addingsToChangingEl: NUMBER. Shift to top or bot.
   let relativeH;
   if (typeof relativeWhich === 'number') {
     relativeH = relativeWhich;
   } else { relativeH = $(relativeWhich).height() }
+
+  if (offsetRelatMiddleView === null) offsetRelatMiddleView = 0;
+
+  if (typeof addingsToChangingEl === 'object') {
+    addingsToChangingEl = $(addingsToChangingEl).height() +
+      findPadVert(addingsToChangingEl) + findMargVert(addingsToChangingEl);
+  }
+  const futureH = $(sizingEl).height() + addingsToChangingEl;
 
   const differH = relativeH - futureH;
   $(sizingEl).css({
@@ -17,6 +31,6 @@ const heightSizingByPad = (sizingEl, relativeWhich, offsetRelatMiddleView = 0, a
 }
 
 $(function() {
-  heightSizingByPad($('.header'), $(window), 20)
+  heightSizingByPad($('.jumbotron'), $(window), 20)
     // let a = $('#work').offset().top;
 })
